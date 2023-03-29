bbe2b6acaab1eefb16bc18887f30f7be9ebcac23
Lindrit <lindritsulaj2.0@gmail.com>
2023-03-29 18:46:11 +0200

    Hero section

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,6 +5,11 @@ export default function Navbar() {
     <nav className='h-[70px] lg:h-[80px] fixed top-0 bg-rich-black w-full'>
       <div className='container mx-auto flex items-center justify-between h-full'>
         <img src="https://ik.imagekit.io/0s9lwb2yr/Developer_Portfolio_-_Lindrit_Sulaj/Images/Logo_-_Without_removed_bg-removebg-preview_tb2PaP98M.png?updatedAt=1680105709945" alt="Logo" className='h-[40px]' />
+        <div className='h-[70%] flex flex-col justify-center gap-[6px]'>
+          <div className='w-8 h-[2px] rounded-lg bg-white'></div>
+          <div className='w-8 h-[2px] rounded-lg bg-white'></div>
+          <div className='w-8 h-[2px] rounded-lg bg-white'></div>
+        </div>
       </div>
     </nav>
   )
