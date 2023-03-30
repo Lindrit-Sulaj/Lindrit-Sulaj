62f97ccb0451b22cb1c2a15ecdd410b5660a7bf6
Lindrit <lindritsulaj2.0@gmail.com>
2023-03-30 22:17:21 +0200

    Fixed padding on mobile

--- sections/Landing/Hero.jsx
+++ sections/Landing/Hero.jsx
@@ -4,7 +4,7 @@ import { Stats } from '@/components';
 export default function Hero() {
   return (
     <section>
-      <div className="hero bg-byzantine-blue py-28 md:py-32 px-10 lg:px-6 xl:px-0 mt-[70px]">
+      <div className="hero bg-byzantine-blue py-20 md:py-32 px-10 lg:px-6 xl:px-0 mt-[70px]">
         <div className="mx-auto max-w-screen-web">
           <h1 className='text-3xl md:text-4xl leading-10 lg:text-6xl font-semibold lg:leading-[70px]'>Designing Web Solutions that Bring Your Ideas to Life</h1>
           <p className='text-lg md:text-xl text-text-blue mt-6 max-w-3xl'>I'm a front-end web developer based in Kosovo, with a passion for creating visually stunning websites using the latest and greatest web technologies. </p>
