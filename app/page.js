fe50bd0f2fbb0418878d09b447c49c05dea835ca
Lindrit <lindritsulaj2.0@gmail.com>
2023-04-06 14:38:17 +0200

    metadata update

--- app/page.js
+++ app/page.js
@@ -8,6 +8,19 @@ import Contact from "@/sections/Landing/Contact";
 export const metadata = {
   title: 'Lindrit Sulaj | Web Developer',
   description: "I'm a 14-year-old web developer based in Kosovo. I specialize in building visually stunning and impressive websites using languages and frameworks such as HTML, CSS, SCSS, Tailwind, React, Next, Redux, and Framer Motion.",
+  openGraph: {
+    title: 'Lindrit Sulaj | Web Developer',
+    description: "I'm a front-end web developer based in Kosovo, with a passion for creating visually stunning websites using the latest and greatest web technologies.",
+    siteName: 'Lindrit Sulaj',
+    type: 'website',
+    images: [
+      {
+        url: 'https://ik.imagekit.io/0s9lwb2yr/Developer_Portfolio_-_Lindrit_Sulaj/Images/Logo_-_Without_removed_bg_jcIzY2POpR.png?updatedAt=1680784572023',
+        width: 400,
+        height: 150
+      }
+    ]
+  }
 }
 
 export default function Home() {
