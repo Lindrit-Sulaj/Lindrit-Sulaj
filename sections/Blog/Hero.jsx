eae0963b94a1f1ce1cae1dd92a545d98b3a32570
Lindrit <lindritsulaj2.0@gmail.com>
2023-04-13 17:38:18 +0200

    ...

--- sections/Blog/Hero.jsx
+++ sections/Blog/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <section className='mt-[70px] bg-byzantine-blue pt-10 pb-20 flex justify-center flex-col items-center px-6 md:px-20'>
+    <section className='mt-[70px] bg-byzantine-blue pt-10 pb-20 flex justify-center flex-col items-center px-12 md:px-20'>
       <h1 className='text-white text-[27px] text-center md:text-4xl leading-10 lg:text-5xl font-semibold lg:leading-[70px] underline underline-offset-4'>My Blog</h1>
       <p className='text-center text-lg md:text-xl text-text-blue mt-5 max-w-xl'>Expand your knowledge on SEO, web development, and more with my blog's diverse articles.</p>
       <div className=' mt-6 w-full max-w-xl mx-auto flex justify-center md:gap-4 gap-2 px-4'>
