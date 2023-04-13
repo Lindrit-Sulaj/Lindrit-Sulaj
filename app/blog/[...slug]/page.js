339f15947fd58d3a38212a3118817ad072bace87
Lindrit <lindritsulaj2.0@gmail.com>
2023-04-13 17:18:37 +0200

    Blog For Mobile

--- app/blog/[...slug]/page.js
+++ app/blog/[...slug]/page.js
@@ -32,13 +32,13 @@ export default async function ArticlePage({ params }) {
   return (
     <main className="mt-[70px]">
       <div className="bg-byzantine-blue">
-        <div className="max-w-screen-web mx-auto  pt-10 pb-20 flex justify-center flex-col items-center px-6 md:px-20 text-white">
+        <div className="max-w-screen-web mx-auto  pt-10 pb-8 md:pb-20 flex justify-center flex-col items-center px-2 md:px-20 text-white">
           <p className={`${sourceSans.className} font-semibold tracking-wide uppercase border-solid border-[1px] px-3 py-[6px] border-blue-500`}>{category.title}</p>
           <h1 className="text-[27px] text-center md:text-3xl leading-10 lg:text-[44px] font-semibold lg:leading-[70px] mt-3 ">{title}</h1>
           <p className="mt-3 text-text-blue text-lg">Updated at: {_updatedAt.slice(0, 10)}</p>
         </div>
       </div>
-      <div className="flex gap-10 px-10 py-12 xl:px-0 max-w-screen-web mx-auto">
+      <div className="flex gap-10 px-7 py-7 md:px-10 md:py-12 xl:px-0 max-w-screen-web mx-auto">
         <div className="w-full md:w-[70%] text-lg portable_text">
           <PortableText value={body} components={portableTextComponents} />
         </div>
