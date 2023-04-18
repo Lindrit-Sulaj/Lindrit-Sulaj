4a6d8f552349832a598304a96aa41a454d8f32b3
Lindrit <lindritsulaj2.0@gmail.com>
2023-04-18 20:15:20 +0200

    Added Sitemap.js and Robots.js

---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.lindritsulaj.com/',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.lindritsulaj.com/blog/',
+      lastModified: new Date(),
+    }
+  ]
+}
