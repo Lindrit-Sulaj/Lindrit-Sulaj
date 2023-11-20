65b4977d92d514d6e0e5cbbb930c2b450b55ee53
Lindrit <94562266+Lindrit-Sulaj@users.noreply.github.com>
2023-11-20 20:21:16 +0100

    Update About.jsx

--- sections/Landing/About.jsx
+++ sections/Landing/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
       <div className='max-w-screen-web mx-auto py-20 md:py-32 px-6 md:px-10 lg:px-6 xl:px-0 flex justify-center md:justify-between flex-wrap md:flex-nowrap gap-x-10 gap-y-14 items-center'>
         <div className='w-full md:w-1/2'>
           <h2 className='text-3xl md:text-4xl lg:text-[44px] font-semibold mt-4'>About Me</h2>
-          <p className='text-[17px] mt-6 lg:text-lg text-neutral-600'>Hi there, I'm a 15-year-old web developer based in Kosovo. I discovered my passion for programming two years ago and have been working tirelessly to improve my skills ever since. In my free time I like to create visually stunning websites.</p>
+          <p className='text-[17px] mt-6 lg:text-lg text-neutral-600'>Hi there, I'm a web developer & designer based in Kosovo. I discovered my passion for programming and seo three years ago and have been working tirelessly to improve my skills ever since. Lately, I also co-founded a web development & seo agency that helps businesses grow online.</p>
           <p className='text-[17px] lg:text-lg mt-3 text-neutral-600'>I'm proficient in a number of languages and frameworks. While I may be young, I am dedicated, hardworking, and committed to delivering exceptional web development solutions. So if you're looking for a talented and passionate web developer to bring your ideas to life, I'd love to hear from you!</p>
           <p className='text-[17px] lg:text-lg mt-6 text-neutral-600'>Here are a few technologies I've been working with recently:</p>
           <div className="inline-grid gap-x-4 gap-y-1 grid-cols-2 mt-4 text-persian-blue font-medium">
